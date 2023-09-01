37d285c75b3e1a44b459e8da1d809f9992b8d14d
Calvn <mlaycalvn@gmail.com>
2023-09-01 12:14:26 +0300

    adding swipe down to reload

--- app/src/main/java/com/chuitec/webview/MainActivity.kt
+++ app/src/main/java/com/chuitec/webview/MainActivity.kt
@@ -4,30 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.activity.OnBackPressedCallback
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var wb_webView: WebView
+    private lateinit var swipeToRefresh: SwipeRefreshLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        swipeToRefresh = findViewById(R.id.swipeToRefresh)
+
+        refreshApp()
 
         wb_webView = findViewById<WebView>(R.id.wb_webView)
         webViewSetup()
     }
 
 
+    //swipe down to refresh the webview
+    private fun refreshApp(){
+        swipeToRefresh.setOnRefreshListener {
+            swipeToRefresh.isRefreshing = false
+            wb_webView.reload()
+        }
+    }
+
+
+    //webview implementation
     private fun webViewSetup() {
         wb_webView.webViewClient = WebViewClient()
         wb_webView.apply {
             loadUrl("https://www.google.com/")
             settings.javaScriptEnabled = true
+            settings.allowFileAccess = true
         }
     }
 
-
+    //stopping back button from existing the app. to be used to go back
     override fun onBackPressed() {
         if (wb_webView.canGoBack()) {
             wb_webView.goBack()
